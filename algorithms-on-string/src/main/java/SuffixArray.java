ee9d8747aeed8c7744090a3ff58020fa28515411
shibayu36 <shibayu36@gmail.com>
2016-12-30 23:25:48 +0900

    SuffixArrayでsubstringしない

--- algorithms-on-string/src/main/java/SuffixArray.java
+++ algorithms-on-string/src/main/java/SuffixArray.java
@@ -1,38 +1,43 @@
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SuffixArray {
-    public static List<Integer> make(String string) {
-        // 部分文字列とindexの集合を作る
-        List<SuffixArrayItem> items = new ArrayList<>();
-        for (int position = 0; position < string.length(); position++) {
-            items.add(new SuffixArrayItem(position, string.substring(position)));
+
+    public static List<Integer> make(String text) {
+        int n = text.length();
+        Suffix[] suffixes = new Suffix[n];
+        for (int i = 0; i < n; i++) {
+            suffixes[i] = new Suffix(text, i);
         }
-
-        // 部分文字列でsortする
-        items.sort( Comparator.comparing(SuffixArrayItem::getString) );
-
-        // indexにmapして返す
-        return items.stream().map(x -> x.getPosition()).collect(Collectors.toList());
+        Arrays.sort(suffixes);
+        return Arrays.stream(suffixes).map(s -> s.index).collect(Collectors.toList());
     }
+
 }
 
-class SuffixArrayItem {
-    private int position;
-    private String string;
+class Suffix implements Comparable<Suffix> {
+    public String text;
+    public int index;
 
-    public SuffixArrayItem(int position, String string) {
-        this.position = position;
-        this.string = string;
+    public Suffix(String text, int index) {
+        this.text = text;
+        this.index = index;
+    }
+    public int length() {
+        return text.length() - index;
+    }
+    public char charAt(int i) {
+        return text.charAt(index + i);
     }
 
-    public int getPosition() {
-        return this.position;
-    }
-
-    public String getString() {
-        return this.string;
+    public int compareTo(Suffix that) {
+        if (this == that) return 0;  // optimization
+        int n = Math.min(this.length(), that.length());
+        for (int i = 0; i < n; i++) {
+            if (this.charAt(i) < that.charAt(i)) return -1;
+            if (this.charAt(i) > that.charAt(i)) return +1;
+        }
+        return this.length() - that.length();
     }
 }
