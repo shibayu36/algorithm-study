feb1ab31022d932f25e23441df419a8651f2683b
shibayu36 <shibayu36@gmail.com>
2016-12-31 12:22:17 +0900

    randomString

--- algorithms-on-string/src/test/java/LCPArrayBenchmark.java
+++ algorithms-on-string/src/test/java/LCPArrayBenchmark.java
@@ -1,6 +1,7 @@
 import static java.lang.System.currentTimeMillis;
 
 import java.util.List;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
@@ -8,7 +9,7 @@ public class LCPArrayBenchmark {
   @Test
   public void make() throws Exception {
     List<Integer> suffixArray, lcpArray;
-    String text = StringUtils.repeat("bananapanama", 1000);
+    String text = StringUtils.repeat("bananapanama", 10000);
     suffixArray = SuffixArray2.make(text);
 
     // warmup
@@ -16,6 +17,17 @@ public class LCPArrayBenchmark {
     LCPArray2.make(text, suffixArray);
 
     long begin = currentTimeMillis();
+    LCPArray1.make(text, suffixArray);
+    System.out.println("lcp array1: " + (currentTimeMillis() - begin));
+
+    begin = currentTimeMillis();
+    LCPArray2.make(text, suffixArray);
+    System.out.println("lcp array2: " + (currentTimeMillis() - begin));
+
+    text = RandomStringUtils.randomAlphabetic(100000);
+    suffixArray = SuffixArray2.make(text);
+
+    begin = currentTimeMillis();
     LCPArray1.make(text, suffixArray);
     System.out.println("lcp array1: " + (currentTimeMillis() - begin));
 
