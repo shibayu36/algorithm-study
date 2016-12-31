408177d9e7eeb471da971ea9825e6f25bd4eb6e1
shibayu36 <shibayu36@gmail.com>
2016-12-31 12:15:11 +0900

    LCP benchmark

---
+++ algorithms-on-string/src/test/java/LCPArrayBenchmark.java
@@ -0,0 +1,26 @@
+import static java.lang.System.currentTimeMillis;
+
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+public class LCPArrayBenchmark {
+  @Test
+  public void make() throws Exception {
+    List<Integer> suffixArray, lcpArray;
+    String text = StringUtils.repeat("bananapanama", 1000);
+    suffixArray = SuffixArray2.make(text);
+
+    // warmup
+    LCPArray1.make(text, suffixArray);
+    LCPArray2.make(text, suffixArray);
+
+    long begin = currentTimeMillis();
+    LCPArray1.make(text, suffixArray);
+    System.out.println("lcp array1: " + (currentTimeMillis() - begin));
+
+    begin = currentTimeMillis();
+    LCPArray2.make(text, suffixArray);
+    System.out.println("lcp array2: " + (currentTimeMillis() - begin));
+  }
+}
