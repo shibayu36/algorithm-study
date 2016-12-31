9a1696b49ff628798f087e9266bfb9695635ffdb
shibayu36 <shibayu36@gmail.com>
2016-12-31 14:12:37 +0900

    benchmark

--- algorithms-on-string/src/test/java/LCPArrayBenchmark.java
+++ algorithms-on-string/src/test/java/LCPArrayBenchmark.java
@@ -1,5 +1,8 @@
 import static java.lang.System.currentTimeMillis;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -8,31 +11,50 @@ import org.junit.Test;
 public class LCPArrayBenchmark {
   @Test
   public void make() throws Exception {
+    long begin;
     List<Integer> suffixArray, lcpArray;
-    String text = StringUtils.repeat("bananapanama", 10000);
+    String text = StringUtils.repeat("bananapanama", 1000);
     suffixArray = SuffixArray2.make(text);
 
     // warmup
     LCPArray1.make(text, suffixArray);
     LCPArray2.make(text, suffixArray);
 
-    long begin = currentTimeMillis();
-    LCPArray1.make(text, suffixArray);
+    begin = currentTimeMillis();
+    lcpArray = LCPArray1.make(text, suffixArray);
     System.out.println("lcp array1: " + (currentTimeMillis() - begin));
 
     begin = currentTimeMillis();
-    LCPArray2.make(text, suffixArray);
+    lcpArray = LCPArray2.make(text, suffixArray);
     System.out.println("lcp array2: " + (currentTimeMillis() - begin));
 
     text = RandomStringUtils.randomAlphabetic(100000);
     suffixArray = SuffixArray2.make(text);
 
     begin = currentTimeMillis();
-    LCPArray1.make(text, suffixArray);
+    lcpArray = LCPArray1.make(text, suffixArray);
     System.out.println("lcp array1: " + (currentTimeMillis() - begin));
 
     begin = currentTimeMillis();
-    LCPArray2.make(text, suffixArray);
+    lcpArray = LCPArray2.make(text, suffixArray);
+    System.out.println("lcp array2: " + (currentTimeMillis() - begin));
+
+    // 指定のファイル URL のファイルをバイト列として読み込む
+    try {
+      byte[] fileContentBytes = Files.readAllBytes(Paths.get("./sample.txt"));
+// 読み込んだバイト列を UTF-8 でデコードして文字列にする
+      text = new String(fileContentBytes, StandardCharsets.UTF_8);
+    }
+    catch (Exception e) {}
+    suffixArray = SuffixArray2.make(text);
+
+    System.out.println("---------------");
+    begin = currentTimeMillis();
+    lcpArray = LCPArray1.make(text, suffixArray);
+    System.out.println("lcp array1: " + (currentTimeMillis() - begin));
+
+    begin = currentTimeMillis();
+    lcpArray = LCPArray2.make(text, suffixArray);
     System.out.println("lcp array2: " + (currentTimeMillis() - begin));
   }
 }
